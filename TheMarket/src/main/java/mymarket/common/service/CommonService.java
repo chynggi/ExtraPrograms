56be0f452f1efc203e2896631ba6acf1b603e303
최용규 <chynggi69@gmail.com>
2019-12-23 14:18:23 +0900

    2.0.0

---
+++ TheMarket/src/main/java/mymarket/common/service/CommonService.java
@@ -0,0 +1,10 @@
+package mymarket.common.service;
+
+import java.util.Map;
+
+public interface CommonService {
+
+	Map<String, Object> selectFileInfo(Map<String, Object> map) throws Exception;
+
+	
+}
--- TheMarket/src/main/java/mymarket/sample/dao/SampleDAO.java
+++ TheMarket/src/main/java/mymarket/sample/dao/SampleDAO.java
@@ -10,6 +10,49 @@ import mymarket.common.dao.AbstractDAO;
 public class SampleDAO extends AbstractDAO {
 
 	public List<Map<String, Object>> selectBoardList(Map<String, Object> map) {
-		return (List<Map<String, Object>>)selectList("sample.selectBoardList", map);
+		return (List<Map<String, Object>>) selectList("sample.selectBoardList", map);
 	}
+
+	public void insertBoard(Map<String, Object> map) {
+		insert("sample.insertBoard", map);
+
+	}
+
+	public void updateHitCnt(Map<String, Object> map) throws Exception {
+		update("sample.updateHitCnt", map);
+	}
+
+	@SuppressWarnings("unchecked")
+	public Map<String, Object> selectBoardDetail(Map<String, Object> map) throws Exception {
+		return (Map<String, Object>) selectOne("sample.selectBoardDetail", map);
+	}
+
+	public void updateBoard(Map<String, Object> map) throws Exception {
+		update("sample.updateBoard", map);
+
+		
+	}
+
+	public void deleteBoard(Map<String, Object> map) throws Exception{
+		update("sample.deleteBoard", map);
+		
+	}
+
+	public void insertFile(Map<String, Object> map) {
+		insert("sample.insertFile", map);
+		
+	}
+
+	public List<Map<String, Object>> selectFileList(Map<String, Object> map) {
+		return (List<Map<String, Object>>)selectList("sample.selectFileList", map);
+	}
+
+	public void deleteFileList(Map<String, Object> map) {
+		update("sample.deleteFileList", map);		
+	}
+
+	public void updateFile(Map<String, Object> map) {
+		update("sample.updateFile", map);		
+	}
+
 }
